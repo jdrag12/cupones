07ab3ac7e94780a0f2c42a86e963b5a5473ff7b9
Joan Drago <jdrago10@gmail.com>
2025-10-21 12:08:50 +0200

    fix vercel v

--- api/redeem.js
+++ api/redeem.js
@@ -154,4 +154,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Error redeeming coupon" });
   }
 }
-
--- api/seed.js
+++ api/seed.js
@@ -171,4 +171,3 @@ export default async function handler(req, res) {
     return res.status(500).json({ error: "Error seeding coupons data" });
   }
 }
-
